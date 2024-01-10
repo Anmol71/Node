d4f8fba7706c33eea400247489b8d3551316bcb4
Blaze Anmol <blaze.anmol@rubicotech.in>
2024-01-10 12:05:56 +0530

    add createdAt & updatedAt column

--- migrations/2024.01.09T12.04.27.CreateUser.ts
+++ migrations/2024.01.09T12.04.27.CreateUser.ts
@@ -30,6 +30,16 @@ export const up: Migration = async ({ context: sequelize }) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP()"),
+      allowNull: false,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP()"),
+      allowNull: false,
+    },
   });
 };
 
---
+++ src/controller/auth.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from "express";
+import { User } from "../models/index";
+
+export async function Register(req: Request, res: Response) {
+  // get required variables from request body
+  // using es6 object destructing
+  const { name, email, password } = req.body;
+  try {
+    // create an instance of a user
+    const newUser = new User({
+      name,
+      email,
+      password,
+    });
+    // Check if user already exists
+    const existingUser = await User.findOne({
+      where: { email },
+    });
+    if (existingUser)
+      return res.status(400).json({
+        status: "failed",
+        data: [],
+        message: "It seems you already have an account, please log in instead.",
+      });
+    const savedUser = await newUser.save(); // save new user into the database
+    // const { role, ...user_data } = savedUser._doc;
+    res.status(200).json({
+      status: "success",
+    //   data: [user_data],
+      message:
+        "Thank you for registering with us. Your account has been successfully created.",
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      code: 500,
+      data: [],
+      message: "Internal Server Error",
+    });
+  }
+  res.end();
+}
